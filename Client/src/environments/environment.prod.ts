8cd4310697baa825d8fdfdd248ca3c863b69f57d
Cauã Rinaldi <cauarinaldi451@gmail.com>
2022-05-11 15:30:43 -0300

    Prepare the app for production

--- Client/src/environments/environment.prod.ts
+++ Client/src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
 export const environment = {
-  production: true
+    production: true,
+    firebase: {
+        apiKey: "public",
+        databaseURL: "https://aiplayground-708f1-default-rtdb.firebaseio.com/",
+        projectId: "aiplayground-708f1",
+        storageBucket: "gs://aiplayground-708f1.appspot.com"
+    }
 };
